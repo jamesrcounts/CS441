1c690b4b4389fb0973d9d15b690105429a05539b
jcounts <devnull@localhost>
2011-10-16 10:48:19 -0700

    Fixed-up AssemblyInfo.  Incremented major version number. Fixed-up project properties.

--- PhotoBuddy/Properties/AssemblyInfo.cs
+++ PhotoBuddy/Properties/AssemblyInfo.cs
@@ -1,16 +1,21 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿//-----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Gold Rush">
+//     Copyright (c) Gold Rush 2011. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TheNewPhotoBuddy")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("PhotoBuddy")]
+[assembly: AssemblyDescription("A photo album designed as a group project for CS441 at CSUSM")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("TheNewPhotoBuddy")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCompany("Gold Rush")]
+[assembly: AssemblyProduct("PhotoBuddy")]
+[assembly: AssemblyCopyright("Copyright © Gold Rush 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +37,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.6")]
-[assembly: AssemblyFileVersion("1.0.0.6")]
+[assembly: AssemblyVersion("2.0.*")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
